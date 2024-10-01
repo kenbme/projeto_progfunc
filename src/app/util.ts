b5488ee70257d8a673ac119ddda870f3e5654068
guilhermefpeixoto <guilherme.peixoto@ccc.ufcg.edu.br>
2024-10-01 11:07:01 -0300

    test: creates tests to util functions

--- src/app/util.ts
+++ src/app/util.ts
@@ -58,6 +58,47 @@ export function compose<A, B, C>(f1: (arg: B) => C, f2: (arg: A) => B): (arg: A)
 }
 
 export function runTests(){
-    
+    const prs = [
+      { id: 1, author: "João", status: "open", amountComments: 10 },
+      { id: 2, author: "Maria", status: "closed", amountComments: 5 },
+      { id: 3, author: "João", status: "merged", amountComments: 12 },
+      { id: 4, author: "Ana", status: "open", amountComments: 7 },
+    ]
+
+    console.log("Lista original dos pull requests para testes:")
+    console.log(prs)
+
+    const distinctPullRequests = distinct(prs, "author");
+    console.log("Resultado da aplicação da função distinct por autor:")
+    console.log(distinctPullRequests)
+
+    const pullRequestsByStatus = groupBy(prs, (pr) => pr.status);
+    console.log("Resultado da aplicação da função groupBy por status")
+    console.log(pullRequestsByStatus)
+
+    const sumComments = fold(
+      (acc, pr) => acc + pr.amountComments,
+      0,
+      prs
+    );
+
+    console.log("Resultado da aplicação da função fold somando a quantidade total de comentários:")
+    console.log(sumComments)
+
+    const totalComments = (pulls: { amountComments: number }[]) =>
+    fold((acc, pr) => acc + pr.amountComments, 0, pulls);
+
+    const meanComments = (total: number, qtdPRs: number) => total / qtdPRs;
+
+    const calculateMeanComments = compose(
+      (total: number) => meanComments(total, prs.length),
+      totalComments
+    );
+
+    const mediaComentariosPR = calculateMeanComments(prs);
+
+    console.log("Resultado da aplicação da função compose para calcular a média de comentários por pull request")
+    console.log(mediaComentariosPR);
+
 }
 
